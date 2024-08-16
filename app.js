6c9189727ca1db583ca7ab5a07325226038c5c5a
judaroqui <juan.rodriguezq@comunidad.iush.edu.co>
2024-08-16 02:32:35 -0500

    Logic Update

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// Textos
+const texts = {
+    title: "Encriptador de Texto",
+    infoText: "El encriptador transforma mayusculas<br> en minusculas y remueve acentos<br> sin problema.",
+    noMessage: `
+        <b>Ningún mensaje fue encontrado.</b><br>
+        <br>Ingresa el texto que desees encriptar o desencriptar.
+    `,
+    invalidInput: `
+        <b>Los números y caracteres especiales<br>no son permitidos.</b>
+        <br><br>Ingresa el texto que desees encriptar o desencriptar.
+    `,
+    encryptButton: "Encriptar",
+    decryptButton: "Desencriptar",
+    inputPlaceholder: "Ingrese el texto aqui...",
+    footerButton: "Juan David Rodriguez"
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.title = texts.title;
+    document.querySelector('h1').innerHTML = `${texts.title} <span class="info-container">
+        <img src="img/info.svg" alt="Info">
+        <span class="info-text">${texts.infoText}</span>
+    </span>`;
+    document.getElementById('no-message').innerHTML = texts.noMessage;
+    document.getElementById('invalid-input').innerHTML = texts.invalidInput;
+    document.getElementById('encrypt-button').textContent = texts.encryptButton;
+    document.getElementById('decrypt-button').textContent = texts.decryptButton;
+    document.getElementById('input-text').placeholder = texts.inputPlaceholder;
+    document.getElementById('footer-button').textContent = texts.footerButton;
+});
+
 // Función para eliminar caracteres especiales y números
 function removeSpecialChatAndNum(text) {
     return text.replace(/[^a-zA-Z\s]/g, '');
@@ -13,7 +45,7 @@ function toLowerCase(text) {
     return text.toLowerCase();
 }
 
-// Función combinada para limpiar el texto
+// Función para limpiar el texto
 function cleanText(text) {
     text = removeAccents(text);
     text = removeSpecialChatAndNum(text);
@@ -48,13 +80,14 @@ function isInvalidInput(text) {
     return /^[^a-zA-Z]*$/.test(text);
 }
 
+// Verifica si el texto no contiene la clave encriptada
 function textWithoutEncriptedKey(text) {
     const cleanedText = cleanText(text);
     const decryptedText = decryptText(cleanedText);
     return cleanedText === decryptedText;
 }
 
-
+// Evento para encriptar y desencriptar
 document.getElementById('encrypt-button').addEventListener('click', function () {
     const inputText = document.getElementById('input-text').value.trim();
     const noMessage = document.getElementById('no-message');
@@ -103,6 +136,7 @@ document.getElementById('decrypt-button').addEventListener('click', function () 
     }
 });
 
+// Evento para copiar el texto
 document.getElementById('copy-button').addEventListener('click', function () {
     const resultText = document.querySelector('.result-text').innerText;
 
@@ -112,10 +146,12 @@ document.getElementById('copy-button').addEventListener('click', function () {
     });
 });
 
+// Evento para dark mode
 document.getElementById('toggle-dark-mode').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
 
+// Evento para abrir el perfil de LinkedIn
 document.addEventListener('DOMContentLoaded', function() {
     const linkedinButton = document.getElementById('linkedin-button');
     linkedinButton.addEventListener('click', function() {
